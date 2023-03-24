91a230ca3d6d10215c1551b79948802a307190d4
vitviki <vtyagi1991@gmail.com>
2023-03-24 15:31:30 +0530

    added: NOTE MODEL

--- notes_backend/api/models.py
+++ notes_backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"{self.body}[0:11]"
