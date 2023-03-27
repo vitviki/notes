e8146073962ecbcc33bdc59e60fc00bdf5f76ae3
vitviki <vtyagi1991@gmail.com>
2023-03-27 17:54:39 +0530

    added: URL ROUTING

--- notes_frontend/src/App.js
+++ notes_frontend/src/App.js
@@ -1,15 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 import './App.css';
 
 import Header from './components/Header';
 import NotesListPage from './scenes/NotesListPage';
+import NotePage from "./scenes/NotePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" exact component={NotesListPage} />
+        <Route path="/note/:id" component={NotePage} />
+      </div>
+    </Router>
+    
   );
-}
+};
 
 export default App;
